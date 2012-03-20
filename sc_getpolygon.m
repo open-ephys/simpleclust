75cbcd5433f9d9071b0ab4c81d8f24c5278076b6
Jakob Voigts <jvoigts@mit.edu>
2012-03-20 17:32:15 -0400

    chenged color of cluster selection poly to match cluster

--- sc_getpolygon.m
+++ sc_getpolygon.m
@@ -1,4 +1,4 @@
-function  [px,py] =getpolygon;
+function  [px,py] =getpolygon(plotcolor);
 
 px=[];
 py=[];
@@ -11,7 +11,7 @@ py(end+1)=y;
 while b~=3
     
     [x,y,b] = ginput(1);
-    plot( [px(end),x],[py(end),y] );
+    plot( [px(end),x],[py(end),y] ,'color',plotcolor);
     
     px(end+1)=x;
     py(end+1)=y;
