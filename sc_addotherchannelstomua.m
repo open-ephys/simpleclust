142e86e52430e263e5936931ba2f71c708912140
Jakob Voigts <jvoigts@mit.edu>
2012-04-01 13:09:38 -0400

    improved performance of multi channel overlap code, can now choose between cpu and ram load to some degree d_t threshold can be changed now, default .5ms

--- sc_addotherchannelstomua.m
+++ sc_addotherchannelstomua.m
@@ -26,44 +26,72 @@ end;
 
 
 features.numextrafeaatures=features.numextrafeaatures+1;
-features.name{size(features.data,1)+1}=['Nchannel spike overlap'];
+features.name{size(features.data,1)+1}=['Ch.overlap'];
 
-tbins=[min(mua.ts):0.5/1000:max(mua.ts)];
+%tbins=[min(mua.ts):0.5/1000:max(mua.ts)]; % make .5ms bins (this will blow up for big files on small machines)
+
+% instead do coarse histogram, use it as lookup table to allow fast spike
+% by spike comparisons
+lookup_binsize = 50; % in ms, smaller -> more ram use, larger -> more cpu time
+tbins=[min(mua.ts):lookup_binsize/1000:max(mua.ts)];
 
 [h_this,spikebins]=histc(mua.ts,tbins);
-Noverlap=h_this.*0;
 
+
+
+h_others=zeros(numel(tbins), numel(mua.otherchannels) );
 for j=1:numel(mua.otherchannels)
-    % h_others(:,j)=histc(mua.otherchannels{j}.ts ,tbins);
-    Noverlap=Noverlap+histc(mua.otherchannels{j}.ts ,tbins).*h_this;
+    [h_others(:,j),otherbins{j}]=histc(mua.otherchannels{j}.ts ,tbins);
 end;
 
-%point the ones that didnt go into the histc(1st and last?) to 0 just in
-%case
-Noverlap(end+1)=0;
-spikebins(spikebins==0)=numel(Noverlap);
-
-features.data(end+1,:)=Noverlap(spikebins);
-
-
-%{
-% way too slow
-mua.diff_to_others=zeros(numel(mua.otherchannels) ,size(features.data,2));
-for i=1:size(features.data,2)
+% now identify overlap for each spike
+lastpercent=0;
+Noverlap=zeros(size(mua.ts));
+for i=1:numel(mua.ts)
+    
     
     if rem(i,100)==0
-    fprintf('%d/%d \n',i,size(features.data,2));
+        percent=round(100*i./length(mua.ts));
+        if percent>lastpercent
+            clf; hold on;
+            fill([-2 -2 5 5],[-2 2 2 -2],'k','FaceColor',[.95 .95 .95]);
+            
+            x=linspace(0,2*pi*percent./100,100);
+            plot(sin(x).*.4,cos(x).*.4,'k','LineWidth',22,'color',[.85 .85 .85])
+            text(0,0,['computing overlap ',num2str(percent),'%']);
+            
+            xlim([-1.3, 3.3]);     ylim([-1.3, 1.2]);
+            daspect([1 1 1]);set(gca,'XTick',[]); set(gca,'YTick',[]);
+            
+            drawnow;
+        end;
+        lastpercent=percent;
     end;
     
-    Noverlap=0;
-    for j=1:numel(mua.otherchannels)
-        mua.diff_to_others(j,i)=  min(abs(mua.otherchannels{j}.ts - mua.ts(i))) ;
+    
+    Noverlap(i)=0;
+    if spikebins(i) >0 % ignore those that are not in histc
+        if sum(h_others(spikebins(i)))>0 % any?
+            for j=1:numel(mua.otherchannels) % for all others
+                if h_others(spikebins(i),j)>0
+                    
+                    otherid = find( otherbins{j}==spikebins(i) ); % find other spikes in same bin
+                    % look also in neighboring bins!
+                    otherid = [otherid; find( otherbins{j}==spikebins(i)+1 )];
+                    otherid = [otherid; find( otherbins{j}==spikebins(i)-1 )];
+                    
+                    if min(abs( mua.ts(i)- mua.otherchannels{j}.ts(otherid) )) < .5/1000; % chance timw window here, or even add penalty based on D_t?
+                        Noverlap(i)=Noverlap(i)+1;
+                    end;
+                    
+                end;
+            end;
+        end;
     end;
-    
-    features.data(end+1,i)=Noverlap;
-    
 end;
 
-%}
+
+
+features.data(end+1,:)=Noverlap;
 
 features=sc_scale_features(features);
