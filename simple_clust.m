8d8bd0f6c52e3f1f869da2c17880dbeafb7c75c3
Jakob Voigts <jvoigts@mit.edu>
2013-01-18 13:18:59 -0500

    made open dialog question work, lets you continue sorting after accidentally clicking open

--- simple_clust.m
+++ simple_clust.m
@@ -20,7 +20,7 @@ X- xcorr feature
 X- display multiple waveforms around where user clicke, like 10ish
 X- allow rescaling of all visible clusters, pretty much works as zoom
 X- time selection at bottom? or just add time feature?
-- add merge clusters function
+X - add merge clusters function
 X- add better features for loading stereotrode and tetrode data
 X- undo function for the cluster operations
 X- make peaks/energy etc work with TTL/ST files
@@ -36,8 +36,8 @@ X - dsplay ch in overlap selection
 X - add progress bar for initial waveform supersampling,
 X - try to improve waveform supersampling speed (do linear?)
 X - do . display by default, not x, increase default displynum to 30000
-- add batch process functions, sorte work in progress
-- figure out saving as ch_simpleclust with no channel nnr bug
+X - add batch process functions, sorte work in progress
+X - figure out saving as ch_simpleclust with no channel nnr bug
 - add warning if ch is not just a number
 X- add isi feature
 X - make add function take spikes only from visible clusters
@@ -56,9 +56,8 @@ global debugstate;
 debugstate = 0; % 0: do nothing, 1: go trough following states
 debuginput = [0 0 0];
 
-%addpath(pwd);
+%addpath(pwd); % warn instead
 %addpath(fullfile(pwd,'read_cheetah'));
-
 
 s_opt = [];
 
@@ -85,7 +84,7 @@ end;
 while run
     
     figure(1); clf; hold on; grid off;
-
+    
     fill([-2 -2 5 5],[-2 2 2 -2],'k','FaceColor',[.92 .92 .92]);
     
     set(gca, 'position', [0 0 1 1]);
@@ -152,65 +151,63 @@ while run
         sc_timeline(features,mua,x,y,b);
         
         % ISI is now plotted for all clusters
-       % if features.selected>0
-       %     sc_plot_cluster_info(features,features.selected);
-       % end;
+        % if features.selected>0
+        %     sc_plot_cluster_info(features,features.selected);
+        % end;
         
         features.highlight = 0; % remove highlight on each click
     end;
     
     
     
-    
     [x,y,b] = sc_ginput(1);
     
     
-
-    if ~dataloaded,
-    if (x<-1)&& (y>0.9) && (y<1) % batch (pre)process
-        
-        [FileName,PathName,FilterIndex] = uigetfile({'*.wf;*.nse;*.nst;*.ntt;','all base electrode file types';'*_simpleclust.mat', 'simpleclust file';'*.mat', 'matlab file';'*.wf','Waveform file';'*.nse' ,'neuralynx single electrode file'; '*.nst',  'neuralynx stereotrode file'; '*.ntt',  'neuralynx tetrode file'},['choose files to preprocess'],'MultiSelect','on');
-
-        
-        if FilterIndex(1)~=0
-        for b=1:numel(FileName)
+    if ~dataloaded
+        if (x<-1)&& (y>0.9) && (y<1) % batch (pre)process
             
-            features.muafile =[PathName,FileName{b}];
+            [FileName,PathName,FilterIndex] = uigetfile({'*.wf;*.nse;*.nst;*.ntt;','all base electrode file types';'*_simpleclust.mat', 'simpleclust file';'*.mat', 'matlab file';'*.wf','Waveform file';'*.nse' ,'neuralynx single electrode file'; '*.nst',  'neuralynx stereotrode file'; '*.ntt',  'neuralynx tetrode file'},['choose files to preprocess'],'MultiSelect','on');
             
-            fprintf('processing file %d of %d \n',b,numel(FileName));
             
-            sc_load_mua_dialog;
-            sc_save_dialog;
-            
-        end;
-        end;
-        dataloaded=0;
-        
-    end;
-    
-
-    if (x<-1)&& (y>0.8) && (y<0.9) % batch run - open folder of simpleclust files and loop trough sorting them one at a time
-        
-        [multifiles,PathName,FilterIndex] = uigetfile({'*_simpleclust.mat', 'simpleclust file';'*.wf;*.nse;*.nst;*.ntt;','all base electrode file types';'*.mat', 'matlab file';'*.wf','Waveform file';'*.nse' ,'neuralynx single electrode file'; '*.nst',  'neuralynx stereotrode file'; '*.ntt',  'neuralynx tetrode file'},['choose files to cluster'],'MultiSelect','on');
-
-        
-        if FilterIndex(1)~=0
-        s_opt.batch=1; % indicate we're doing a batch
-        multi_N=1; % cycle trough many files
-        
-        
-        features.muafile =[PathName,multifiles{multi_N}];
-        sc_load_mua_dialog;
-        else
+            if FilterIndex(1)~=0
+                for b=1:numel(FileName)
+                    
+                    features.muafile =[PathName,FileName{b}];
+                    
+                    fprintf('processing file %d of %d \n',b,numel(FileName));
+                    
+                    sc_load_mua_dialog;
+                    sc_save_dialog;
+                    
+                end;
+            end;
             dataloaded=0;
+            
         end;
         
-    end;
-
+        
+        if (x<-1)&& (y>0.8) && (y<0.9) % batch run - open folder of simpleclust files and loop trough sorting them one at a time
+            
+            [multifiles,PathName,FilterIndex] = uigetfile({'*_simpleclust.mat', 'simpleclust file';'*.wf;*.nse;*.nst;*.ntt;','all base electrode file types';'*.mat', 'matlab file';'*.wf','Waveform file';'*.nse' ,'neuralynx single electrode file'; '*.nst',  'neuralynx stereotrode file'; '*.ntt',  'neuralynx tetrode file'},['choose files to cluster'],'MultiSelect','on');
+            
+            
+            if FilterIndex(1)~=0
+                s_opt.batch=1; % indicate we're doing a batch
+                multi_N=1; % cycle trough many files
+                
+                
+                features.muafile =[PathName,multifiles{multi_N}];
+                sc_load_mua_dialog;
+            else
+                dataloaded=0;
+            end;
+            
+        end;
+        
     end
     
     if dataloaded
-
+        
         features=sc_parse_feature_selection(x,y,features);
         
         features=sc_parse_custerselection(features,x,y,mua);
@@ -221,9 +218,9 @@ while run
         
         features=sc_timeline(features,mua,x,y,b);
         
-
+        
         if y<-1 && y>-1.1
-            features=  sc_extramenu(features,mua,x,y);
+            features=  sc_extramenu(features,mua,x,y,b);
         end;
         
         if (b==3) && (abs(x)<1) && (abs(y)<1)
@@ -232,55 +229,41 @@ while run
     end;
     
     if (x<-1)&& (y>1)
-        
         if y > 1.1
-            
-            
+             
             disp('open');
             
+            
             if s_opt.batch % open next file in batch
-
                 if dataloaded
                     button = questdlg('Save and open next file?','open?','Yes','No','Yes');
                 else
                     button='Yes';
-
                 end;
-                
-                sc_save_dialog;
-                
 
+                sc_save_dialog;
                 multi_N=multi_N+1;
-                
-                
+
                 features.muafile =[PathName,multifiles{multi_N}];
                 sc_load_mua_dialog;
-                
-                
-                
-                
+                  
             else % open one file
                 
                 if dataloaded
-                    button = questdlg('Open new MUA dataset?','open?','Yes','No','Yes');
+                    button = questdlg('Open new MUA dataset?','open?','Open','Continue editing','Open');
                 else
-
-                    button='Yes';
+                    button='Open';
                 end;
-                if strcmp(button,'Yes')
+                if strcmp(button,'Open')
                     
                     % load MUa data
-
-                    global debugstate
+                    
+                    global debugstate;
                     if debugstate > 0
-                        
                         PathName = '/home/jvoigts/Dropbox/em003/good/';
                         FileName =  'ST11.nse';
                     else
-
-
                         [FileName,PathName,FilterIndex] = uigetfile({'*.wf;*.nse;*.nst;*.ntt;','all base electrode file types';'*_simpleclust.mat', 'simpleclust file';'*.mat', 'matlab file';'*.wf','Waveform file';'*.nse' ,'neuralynx single electrode file'; '*.nst',  'neuralynx stereotrode file'; '*.ntt',  'neuralynx tetrode file'},'choose input file');
-                        
                     end;
                     
                     features.muafile =[PathName,FileName];
@@ -288,16 +271,14 @@ while run
                     % could parse filename here but the small time saving is
                     % not worth the loss of flexibility
                     %
-                    
-                    
-                    
                     %   features.muafile='/home/jvoigts/Documents/moorelab/acute_test_may27_2011/data_2011-05-20_00-12-09_oddball/spikes_from_csc/mua_ch5.mat';
                     sc_load_mua_dialog;
-                    
-                else
-                    run=0;
-
-                    return;
+               
+                    % if not, just keep going
+               % else
+               %     run=0;
+               %    
+               %     return;
                 end;
             end;
             
@@ -306,21 +287,18 @@ while run
             disp('save/exit');
             % ask for saving here
             if dataloaded
-                button = questdlg('Save clusters?','save?','Save','Continue editing','Save');
-                
+                button = questdlg('Save clusters?','save?','Save','Continue editing','Save'); 
                 if strcmp(button,'Save')
-                    
                     
                     sc_save_dialog;
                     
-
                     button = questdlg('Quit or load new file?','Quit?','Load new file...','Quit','Load new file...');
                     if strcmpi(button, 'quit'),
                         run=0;
                         continue;
                     else
                         % load MUa data
-                        global debugstate
+                        global debugstate;
                         if debugstate > 0
                             
                             PathName = '/home/jvoigts/Dropbox/em003/good/';
@@ -336,13 +314,10 @@ while run
                         % could parse filename here but the small time saving is
                         % not worth the loss of flexibility
                         %
-                        
-                        
-                        
                         %   features.muafile='/home/jvoigts/Documents/moorelab/acute_test_may27_2011/data_2011-05-20_00-12-09_oddball/spikes_from_csc/mua_ch5.mat';
                         sc_load_mua_dialog;
                     end
-
+                    
                 end;
             else
                 clf; drawnow;
